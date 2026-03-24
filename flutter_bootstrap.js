c95525b1f3dd84d8ce211a0396f200a053e55b84
kabi_rules <javifont@gmail.com>
2026-03-24 22:55:54 +0100

    localstorage

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2846907657"
+    serviceWorkerVersion: "1911725473"
   }
 });
